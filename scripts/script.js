18dec621241e20bb58d1e9a7064d9e908fe7826c
Bruno Bortolli <bbortolli2@hotmail.com>
2019-06-10 21:49:39 -0300

    Adicionado assunto e regex para nome

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,39 @@
 $(document).ready(function() {
     var sent = false;
 
-    function checkRequired() {
-        var name = $("#name").val();
-        var message = $("#message").val();
-        if (!name || !message){  	
+    function checkRequired(name, message, subject) {
+
+        if (!name || !message || !subject){  	
             return false; 
         }  	
         return true; 
     } 
 
+    function checkPattern(name) {
+        var pat = new RegExp(/[a-zA-Z]+/);
+        if (!pat.test(name)){
+            return false;
+        }
+        return true;
+    }
+
     function sendEmail() {
         if (sent){
             return false;
         }
+
+        var name = $("#name").val();
+        var message = $("#message").val();
+        var subject = $("#subject").val();
         
         // Verificar se campos nao sao vazios
-        if (!checkRequired()){
+        if (!checkRequired(name, message, subject)){
             alert("Preencha todos os campos"); 
+            return false;
+        }
+
+        if (!checkPattern(name)){
+            alert("Preencha o nome apenas com letras"); 
             return false;
         }
 
@@ -25,8 +41,9 @@ $(document).ready(function() {
         loaderAni.addClass('show');
 
         var data = {
-            name : $("#name").val(),
-            message : $("#message").val()
+            name : name,
+            message : message,
+            subject : subject
         }
 
         $.ajax({
--- scripts/send.php
+++ scripts/send.php
@@ -5,19 +5,20 @@ function send() {
     $post = file_get_contents('php://input');
     $data = json_decode($post);
     
-    if ( empty($data->name) || empty($data->message) ){
+    if ( empty($data->name) || empty($data->message) || empty($data->subject) ){
         return header("HTTP/1.1 400 Bad Request");
     }
 
     $name = $data->name;
     $message = $data->message;
+    $subject = $data->subject;
 
-    $mail_body = 'Nome: ' . $name . '<br>' . $message; 
+    $mail_body = $name . ' diz,' . '<br>' . $message . '.'; 
 
-    $headers = "From: teste@teste.com\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    //$headers = "From: teste@teste.com\r\n";
+    $headers = "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    if (mail("brunobortolli@outlook.com", "Nova mensagem", $mail_body, $headers) === TRUE){
+    if (mail("brunobortolli@outlook.com", $subject, $mail_body, $headers) === TRUE){
         return header("HTTP/1.1 200 Ok");
     }
     else{
